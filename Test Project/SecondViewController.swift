1cebe4f9e89c6d365ce6c74d5ef799a9d45bfb79
Matthew Teague <jillteague@Matthew-Teague.gateway>
2017-06-19 20:20:44 +1000

    merge commit

---
+++ Test Project/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Test Project
+//
+//  Created by Matthew Teague on 6/19/17.
+//  Copyright © 2017 Matthew Teague. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello")
+        print("Feature x")
+        print("New View")
+        print("Git")
+
+}
+
+}
